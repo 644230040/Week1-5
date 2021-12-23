7f23bff845cf71c47a1a56b126e64f0c7984bc00
Pete Pete <644230040@webmail.npru.ac.th>
2021-12-23 14:13:18 +0700

    java code

---
+++ Week3/RuleOfNmae.java
@@ -0,0 +1,14 @@
+package Week3;
+public class RuleOfNmae {
+    public static void main(String[] args) {
+        
+        final int num1=20;   //ค่าคงที่
+        int num2=30;
+
+        System.out.println(num1);
+        System.out.println(num2);
+
+        //num1=50;
+        System.out.println(num1);
+    }
+}
---
+++ Week3/Strings.java
@@ -0,0 +1,6 @@
+package Week3;
+public class Strings{
+    public static void main(String[] args) {
+        System.out.println("xxxxx");
+    }
+} 
